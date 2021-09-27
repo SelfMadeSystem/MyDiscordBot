b5b925767659c248bbbbe94536e1f719da6cff1a
sms <creepercannyon@gmail.com>
2021-09-27 15:11:36 -0700

    Improved message/userInfo commands I guess

--- src/commands/menu/messageInfo.ts
+++ src/commands/menu/messageInfo.ts
@@ -1,12 +1,32 @@
 import { Command } from '../command';
-import { ContextMenuInteraction, SnowflakeUtil } from 'discord.js'
+import { ContextMenuInteraction, MessageEmbed, TextChannel, BaseGuildTextChannel } from 'discord.js'
 
 const command: Command = {
     name: 'Message Info',
-    menuCommand(interaction: ContextMenuInteraction) {
+    async menuCommand(interaction: ContextMenuInteraction) {
+        const channel = interaction.channel as TextChannel;
+        const message = await channel.messages.fetch(interaction.targetId)
+            .catch((e) => { throw e });
+        const member = await message.guild.members.fetch(message.author.id);
+        const embed = new MessageEmbed()
+            .setTitle(`Message Info`)
+            .setColor(member.displayColor)
+            .setThumbnail(member.user.displayAvatarURL())
+            .addField(`Author`, `${member.user.tag} (${member.id})`)
+            .addField(`Channel`, `${channel.name} (${channel.id})`)
+            .addField(`Content`, message.content.length > 0 ? message.content : 'No content')
+            .addField(`Timestamp`, message.createdAt.toLocaleString())
+            .addField(`Attachments`, message.attachments.size > 0 ? message.attachments.map(attachment => attachment.proxyURL).join('\n') : 'None')
+            .addField(`Embeds`, message.embeds.length > 0 ? `${message.embeds.length}` : 'None')
+            .addField(`Reactions`, message.reactions.cache.size > 0 ? message.reactions.cache.map(reaction => reaction.emoji.name).join('\n') : 'None')
+            .addField(`Mentions`, message.mentions.users.size > 0 ? message.mentions.users.map(user => user.tag).join('\n') : 'None')
+            .addField(`Mentioned Roles`, message.mentions.roles.size > 0 ? message.mentions.roles.map(role => role.name).join('\n') : 'None')
+            .addField(`Mentioned Channels`, message.mentions.channels.size > 0 ? message.mentions.channels.map(channel => (channel as BaseGuildTextChannel).name).join('\n') : 'None')
+            .addField(`Mentioned Everyone`, message.mentions.everyone ? 'Yes' : 'No')
+
         interaction.reply({
-            content: 'Message Info',
-            ephemeral: true
+            embeds: [embed],
+            ephemeral: true,
         })
     },
     discordCommand: {
--- src/commands/menu/userInfo.ts
+++ src/commands/menu/userInfo.ts
@@ -1,13 +1,27 @@
 import { Command } from '../command';
-import { ContextMenuInteraction, SnowflakeUtil } from 'discord.js'
+import { ContextMenuInteraction, MessageEmbed } from 'discord.js'
 
 const command: Command = {
     name: 'User Info',
-    menuCommand(interaction: ContextMenuInteraction) {
+    async menuCommand(interaction: ContextMenuInteraction) {
+        const member = await interaction.guild.members.fetch(interaction.targetId)
+            .catch((e) => { throw e });
+        const embed = new MessageEmbed()
+            .setTitle(`${member.user.username}'s Info`)
+            .setThumbnail(member.user.avatarURL())
+            .setColor('#1AACA7')
+            .addField('ID', member.user.id)
+            .addField('Created At', member.user.createdAt.toLocaleString())
+            .addField('Joined At', member.joinedAt.toLocaleString())
+            .addField('Roles', member.roles.cache.map(r => r.name).join(', '))
+            .addField('Bot', member.user.bot ? 'Yes' : 'No')
+            .addField('Nickname', member.nickname ?? "None")
+            .setAuthor(`Requested by: ${interaction.user.username}`, interaction.user.avatarURL())
         interaction.reply({
-            content: 'User Info',
-            ephemeral: true
-        })
+            // content: `${member} ${interaction.targetId}`,
+            ephemeral: true,
+            embeds: [embed]
+        });
     },
     discordCommand: {
         name: 'User Info',

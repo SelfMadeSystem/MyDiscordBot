1198654c1d5bcd6e8430ab255eb91df49f2904f8
sms <creepercannyon@gmail.com>
2023-02-12 13:42:09 -0800

    Updated deps and switched to bun

--- src/commands/slash/test.ts
+++ src/commands/slash/test.ts
@@ -299,13 +299,12 @@ const paginationTest: SlashCommand = {
             if (ephemeral.ephemeral) {
                 interaction.reply(page);
             } else {
-                const { content, components, embeds, files, attachments } = page;
+                const { content, components, embeds, files } = page;
                 interaction.message.edit({
                     content,
                     components,
                     embeds,
-                    files,
-                    attachments
+                    files
                 })
                 interaction.deferUpdate();
             }
@@ -347,4 +346,4 @@ const anotherTest: SlashCommand = {
     }
 }
 
-export default anotherTest;
+export default paginationTest;
